d823f4ad72d0b3a06014bd860d771664de4c8b42
git-demo <shindeonkar735@gmail.com>
2025-11-21 15:33:51 +0530

    updated code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
 
 function Footer() {
-  return (
-    <div className='container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-20'>
-        <img width={160} src={assets.logo} alt=''/>
-        <p className='flex-1 border-1 border-gray-400 pl-4 text-sm text-gray-400 max-sm:hidden'>copyright © 2024 Job-Portal. All rights reserved.</p>
-        <div className='flex gap-2.5' >
-            <img width={38} src={assets.facebook_icon} alt=''/>
-            <img width={38} src={assets.instagram_icon} alt=''/>
-            <img width={38} src={assets.twitter_icon} alt=''/>
+  function open() {
+    window.open("https://www.facebook.com/", "_blank");
+  }
+  function open1() {
+    window.open("https://www.instagram.com/", "_blank");
+  }
+  function open2() {
+    window.open("https://www.twitter.com/", "_blank");
+  }
+   const scrollToTop = ()=>{
+        window.scrollTo({
+            top:0,
+            behavior:"smooth"
+        })
 
-        </div>
-      
+    }
+  return (
+    <div className="container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-20">
+      <img onClick={scrollToTop} width={160} src={assets.logo} alt="" />
+      <p className="flex-1 border-1 border-gray-400 pl-4 text-sm text-gray-400 max-sm:hidden">
+        copyright © 2024 Job-Portal. All rights reserved.
+      </p>
+      <div className="flex gap-2.5">
+        <img onClick={open} width={38} src={assets.facebook_icon} alt="" />
+        <img onClick={open1} width={38} src={assets.instagram_icon} alt="" />
+        <img onClick={open2} width={38} src={assets.twitter_icon} alt="" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,26 @@ import { Link } from 'react-router-dom';
 
 
 function Navbar() {
+    
     const { openSignIn } = useClerk()
     const {user}=useUser();
+
+    const scrollToBottom=()=>{
+
+         // 1. Get the total height of the document content
+    const documentHeight = document.body.scrollHeight;
+
+    // 2. Scroll the window to that height
+    window.scrollTo({
+      top: documentHeight, // Scroll position is set to the very bottom
+      behavior: 'smooth'   // Makes the scroll animation smooth
+    });
+    }
+   
   return (
     <div className='shadow py-4 bg-black'>
       <div className='container px-4 2x1:px-20 mx-auto flex justify-between items-center'>
-        <img className='bg-white p-1 rounded' src={assets.logo} alt="" />
+        <img onClick={scrollToBottom}  className='bg-white p-1 rounded' src={assets.logo} alt="" />
         {
             user
             ?<div className='flex items-center gap-3'>

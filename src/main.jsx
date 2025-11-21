7a1e3cb62145236941cfc3a7e181081c55373319
git-demo <shindeonkar735@gmail.com>
2025-11-21 17:10:04 +0530

    updated code

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
       <BrowserRouter>
         <AppContextProvider>
           <App />
